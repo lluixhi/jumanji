683f37ee26392455d2faa3349748d5a8b710ce2e
Moritz Lipp <mlq@pwmt.org>
2011-07-04 13:11:35 +0200

    Functions to save bookmarks/history items in the database

--- database.c
+++ database.c
@@ -1,9 +1,19 @@
 /* See LICENSE file for license and copyright information */
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <sqlite3.h>
 
 #include "database.h"
+
+/**
+ * Prepares a sql statement
+ *
+ * @param session The database session
+ * @param statement The sql statement
+ * @return Statement object
+ */
+sqlite3_stmt* db_prepare_statement(db_session_t* session, const char* statement);
 
 typedef struct db_sqlite_s
 {
@@ -13,16 +23,14 @@ typedef struct db_sqlite_s
 static const char SQL_INIT[] =
   /* history table */
   "CREATE TABLE IF NOT EXISTS history ("
-    "id INT PRIMARY KEY,"
-    "url TEXT,"
+    "url TEXT PRIMARY KEY,"
     "title TEXT,"
     "visited INT"
     ");"
 
   /* bookmarks table */
   "CREATE TABLE IF NOT EXISTS bookmarks ("
-    "id INT PRIMARY KEY,"
-    "url TEXT,"
+    "url TEXT PRIMARY KEY,"
     "title TEXT"
     ");";
 
@@ -106,4 +114,109 @@ db_close(db_session_t* session)
   g_free(session->filename);
 
   free(session);
+}
+
+sqlite3_stmt*
+db_prepare_statement(db_session_t* session, const char* statement)
+{
+  if (session == NULL || session->data == NULL || statement == NULL) {
+    return false;
+  }
+
+  db_sqlite_t* sqlite_session = (db_sqlite_t*) session->data;
+  if (sqlite_session->session == NULL) {
+    return false;
+  }
+
+  const char* pz_tail   = NULL;
+  sqlite3_stmt* pp_stmt = NULL;
+
+  if (sqlite3_prepare(sqlite_session->session, statement, -1,
+        &pp_stmt, &pz_tail) != SQLITE_OK) {
+    girara_error("Failed to prepare query: %s", statement);
+    goto error_free;
+  } else if (pz_tail && *pz_tail != '\0') {
+    girara_error("Unused portion of statement: %s", pz_tail);
+    goto error_free;
+  }
+
+  return pp_stmt;
+
+error_free:
+
+  if (pp_stmt != NULL) {
+    sqlite3_finalize(pp_stmt);
+  }
+
+  return NULL;
+}
+
+void
+db_bookmark_add(db_session_t* session, const char* url, const char* title)
+{
+  if (session == NULL || url == NULL || title == NULL || session->data == NULL) {
+    return;
+  }
+
+  /* get database connection */
+  db_sqlite_t* sqlite_session = (db_sqlite_t*) session->data;
+  if (sqlite_session->session == NULL) {
+    return;
+  }
+
+  /* prepare statement */
+  static const char SQL_BOOKMARK_ADD[] =
+    "INSERT INTO bookmarks (url, title) VALUES (?, ?);";
+
+  sqlite3_stmt* statement = db_prepare_statement(session, SQL_BOOKMARK_ADD);
+
+  if (statement == NULL) {
+    return;
+  }
+
+  if (sqlite3_bind_text(statement, 1, url,   -1, NULL) != SQLITE_OK ||
+      sqlite3_bind_text(statement, 2, title, -1, NULL) != SQLITE_OK
+      ) {
+    girara_error("Could not bind query parameters");
+    sqlite3_finalize(statement);
+    return;
+  }
+
+  sqlite3_step(statement);
+  sqlite3_finalize(statement);
+}
+
+void
+db_history_add(db_session_t* session, const char* url, const char* title)
+{
+  if (session == NULL || url == NULL || title == NULL || session->data == NULL) {
+    return;
+  }
+
+  /* get database connection */
+  db_sqlite_t* sqlite_session = (db_sqlite_t*) session->data;
+  if (sqlite_session->session == NULL) {
+    return;
+  }
+
+  /* prepare statement */
+  static const char SQL_HISTORY_ADD[] =
+    "INSERT INTO history (url, title) VALUES (?, ?);";
+
+  sqlite3_stmt* statement = db_prepare_statement(session, SQL_HISTORY_ADD);
+
+  if (statement == NULL) {
+    return;
+  }
+
+  if (sqlite3_bind_text(statement, 1, url,   -1, NULL) != SQLITE_OK ||
+      sqlite3_bind_text(statement, 2, title, -1, NULL) != SQLITE_OK
+      ) {
+    girara_error("Could not bind query parameters");
+    sqlite3_finalize(statement);
+    return;
+  }
+
+  sqlite3_step(statement);
+  sqlite3_finalize(statement);
 }
--- database.h
+++ database.h
@@ -28,4 +28,22 @@ db_session_t* db_open(jumanji_t* session, const char* filename);
  */
 void db_close(db_session_t* session);
 
+/**
+ * Save a new bookmark in the database
+ *
+ * @param session The databases session
+ * @param url The url of the bookmark
+ * @param title The title of the bookmark
+ */
+void db_bookmark_add(db_session_t* session, const char* url, const char* title);
+
+/**
+ * Save a new history item in the database
+ *
+ * @param session The database session
+ * @param url The url of the history item
+ * @param title The title of the history item
+ */
+void db_history_add(db_session_t* session, const char* url, const char* title);
+
 #endif // DATABASE_H
