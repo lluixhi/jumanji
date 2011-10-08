b6d0b46e42cf3b46fa46af3a91bb1fe84acf32fa
Sebastian Ramacher <s.ramacher@gmx.at>
2011-10-08 23:17:26 +0200

    introduce GIRARA_INC/LIB, WEBKIT_INC/LIB and make them overwritable

--- config.mk
+++ config.mk
@@ -8,16 +8,29 @@ PREFIX ?= /usr
 MANPREFIX ?= ${PREFIX}/share/man
 
 # gtk3
-GTK_INC = $(shell pkg-config --cflags gtk+-3.0 webkitgtk-3.0 girara-gtk3)
-GTK_LIB = $(shell pkg-config --libs   gtk+-3.0 webkitgtk-3.0 girara-gtk3)
+GTK_INC ?= $(shell pkg-config --cflags gtk+-3.0)
+GTK_LIB ?= $(shell pkg-config --libs   gtk+-3.0)
+
+WEBKIT_INC ?= $(shell pkg-config --cflags webkitgtk-3.0)
+WEBKIT_LIB ?= $(shell pkg-config --libs   webkitgtk-3.0)
+
+GIRARA_INC ?= $(shell pkg-config --cflags girara-gtk3)
+GIRARA_LIB ?= $(shell pkg-config --libs   girara-gtk3)
 
 # gtk2
-#GTK_INC = $(shell pkg-config --cflags gtk+-2.0 webkit-1.0 girara-gtk2)
-#GTK_LIB = $(shell pkg-config --libs   gtk+-2.0 webkit-1.0 girara-gtk2)
+#GTK_INC ?= $(shell pkg-config --cflags gtk+-2.0)
+#GTK_LIB ?= $(shell pkg-config --libs   gtk+-2.0)
+
+#WEBKIT_INC ?= $(shell pkg-config --cflags webkit-1.0)
+#WEBKIT_LIB ?= $(shell pkg-config --libs   webkit-1.0)
+
+#GIRARA_INC ?= $(shell pkg-config --cflags girara-gtk2)
+#GIRARA_LIB ?= $(shell pkg-config --libs   girara-gtk2)
+
 
 # libs
-INCS = -I. -I/usr/include ${GTK_INC}
-LIBS = -lc ${GTK_LIB} -lpthread
+INCS = -I. ${GTK_INC} ${WEBKIT_INC} ${GIRARA_INC}
+LIBS = -lc ${GTK_LIB} ${WEBKIT_LIB} ${GIRARA_LIB} -lpthread
 
 # flags
 CFLAGS += -std=c99 -pedantic -Wall -Wno-format-zero-length $(INCS)
@@ -29,7 +42,7 @@ DFLAGS = -O0 -g
 CC ?= gcc
 
 # strip
-SFLAGS = -s
+SFLAGS ?= -s
 
 # possible values are: sqlite and plain
 DATABASE ?= plain
