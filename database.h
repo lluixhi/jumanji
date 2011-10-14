bf3441930b4865db0f5b3be54acacd28ec2d9a3b
Moritz Lipp <mlq@pwmt.org>
2011-10-14 21:13:38 +0200

    Reimplement cookie jar in database

--- database.h
+++ database.h
@@ -27,7 +27,7 @@ jumanji_database_t* jumanji_db_init(const char* dir);
  *
  * @param session The database session
  */
-void jumanji_db_free(jumanji_database_t* session);
+void jumanji_db_free(jumanji_database_t* database);
 
 /**
  * Save a new bookmark in the database
@@ -36,7 +36,7 @@ void jumanji_db_free(jumanji_database_t* session);
  * @param url The url of the bookmark
  * @param title The title of the bookmark
  */
-void jumanji_db_bookmark_add(jumanji_database_t* session, const char* url, const char* title);
+void jumanji_db_bookmark_add(jumanji_database_t* database, const char* url, const char* title);
 
 /**
  * Find bookmarks
@@ -45,7 +45,7 @@ void jumanji_db_bookmark_add(jumanji_database_t* session, const char* url, const
  * @param input The data that the bookmark should match
  * @return list or NULL if an error occured
  */
-girara_list_t* jumanji_db_bookmark_find(jumanji_database_t* session, const char* input);
+girara_list_t* jumanji_db_bookmark_find(jumanji_database_t* database, const char* input);
 
 /**
  * Removes a saved bookmark
@@ -53,7 +53,7 @@ girara_list_t* jumanji_db_bookmark_find(jumanji_database_t* session, const char*
  * @param session The database session
  * @param url The url that should be removed
  */
-void jumanji_db_bookmark_remove(jumanji_database_t* session, const char* url);
+void jumanji_db_bookmark_remove(jumanji_database_t* database, const char* url);
 
 /**
  * Save a new history item in the database
@@ -62,7 +62,7 @@ void jumanji_db_bookmark_remove(jumanji_database_t* session, const char* url);
  * @param url The url of the history item
  * @param title The title of the history item
  */
-void jumanji_db_history_add(jumanji_database_t* session, const char* url, const char* title);
+void jumanji_db_history_add(jumanji_database_t* database, const char* url, const char* title);
 
 /**
  * Find history
@@ -71,7 +71,7 @@ void jumanji_db_history_add(jumanji_database_t* session, const char* url, const 
  * @param input The data that the bookmark should match
  * @return list or NULL if an error occured
  */
-girara_list_t* jumanji_db_history_find(jumanji_database_t* session, const char* input);
+girara_list_t* jumanji_db_history_find(jumanji_database_t* database, const char* input);
 
 /**
  * Cleans the history
@@ -79,7 +79,7 @@ girara_list_t* jumanji_db_history_find(jumanji_database_t* session, const char* 
  * @param session The database session
  * @param age The age of the entries in seconds
  */
-void jumanji_db_history_clean(jumanji_database_t* session, unsigned int age);
+void jumanji_db_history_clean(jumanji_database_t* database, unsigned int age);
 
 /**
  * Saves a new quickmark (or overwrites an existing one)
@@ -88,7 +88,7 @@ void jumanji_db_history_clean(jumanji_database_t* session, unsigned int age);
  * @param identifier The quickmark identifier
  * @param url Url the quickmark points to
  */
-void jumanji_db_quickmark_add(jumanji_database_t* session, const char identifier, const char* url);
+void jumanji_db_quickmark_add(jumanji_database_t* database, const char identifier, const char* url);
 
 /**
  * Finds a quickmark
@@ -97,7 +97,7 @@ void jumanji_db_quickmark_add(jumanji_database_t* session, const char identifier
  * @param identifier The quickmark identifier
  * @return The url of the quickmark otherwise NULL
  */
-char* jumanji_db_quickmark_find(jumanji_database_t* session, const char identifier);
+char* jumanji_db_quickmark_find(jumanji_database_t* database, const char identifier);
 
 /**
  * Remove a quickmark
@@ -105,7 +105,41 @@ char* jumanji_db_quickmark_find(jumanji_database_t* session, const char identifi
  * @param session The database session
  * @param identifier The quickmark identifier
  */
-void jumanji_db_quickmark_remove(jumanji_database_t* session, const char identifier);
+void jumanji_db_quickmark_remove(jumanji_database_t* database, const char identifier);
+
+/**
+ * Saves a cookie
+ *
+ * @param session Database session
+ * @param name Name
+ * @param value Value
+ * @param domain Domain
+ * @param path Path
+ * @param expires Expire date
+ * @param secure Secure
+ * @param http_only Http only
+ */
+void jumanji_db_cookie_add(jumanji_database_t* database, const char* name, const char*
+    value, const char* domain, const char* path, time_t expires, bool secure,
+    bool http_only);
+
+/**
+ * Remove a cookie
+ *
+ * @param session Database session
+ * @param domain domain
+ * @param name Name
+ */
+void jumanji_db_cookie_remove(jumanji_database_t* database, const char* domain, const char*
+    name);
+
+/**
+ * Returns a list of all SoupCookie's
+ *
+ * @param session The database session
+ * @return List of cookies or NULL if an error occured
+ */
+girara_list_t* jumanji_db_cookie_list(jumanji_database_t* database);
 
 /**
  * Frees a result link
--- soup.c
+++ soup.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <girara.h>
 
+#include "database.h"
 #include "soup.h"
 
 jumanji_soup_t*
@@ -24,22 +25,19 @@ jumanji_soup_init(jumanji_t* jumanji)
     return NULL;
   }
 
-  char* cookie_file = g_build_filename(jumanji->config.config_dir,
-      JUMANJI_COOKIE_FILE, NULL);
-  if (cookie_file == NULL) {
+  g_signal_connect(G_OBJECT(soup->session), "request-started",
+      G_CALLBACK(cb_jumanji_soup_session_request_started), jumanji);
+
+  soup->cookie_jar = soup_cookie_jar_new();
+  if (soup->cookie_jar == NULL) {
     free(soup);
     return NULL;
   }
 
-  SoupCookieJar* cookie_jar = soup_cookie_jar_text_new(cookie_file, FALSE);
-  if (cookie_jar == NULL) {
-    g_free(cookie_file);
-    free(soup);
-    return NULL;
-  }
-  g_free(cookie_file);
+  g_signal_connect(G_OBJECT(soup->cookie_jar), "changed",
+      G_CALLBACK(cb_jumanji_soup_jar_changed), jumanji);
 
-  soup_session_add_feature(soup->session, (SoupSessionFeature*) cookie_jar);
+  soup_session_add_feature(soup->session, (SoupSessionFeature*) soup->cookie_jar);
 
   return soup;
 }
@@ -52,6 +50,86 @@ jumanji_soup_free(jumanji_soup_t* soup)
   }
 
   free(soup);
+}
+
+void
+cb_jumanji_soup_jar_changed(SoupCookieJar* jar, SoupCookie* old_cookie,
+    SoupCookie* new_cookie, jumanji_t* jumanji)
+{
+  if (jar == NULL || jumanji == NULL || jumanji->database == NULL) {
+    return;
+  }
+
+  SoupCookie* cookie = (new_cookie != NULL) ? new_cookie :
+    ((old_cookie != NULL) ? old_cookie : NULL);
+  if (cookie == NULL) {
+    return;
+  }
+
+  const char* name   = soup_cookie_get_name(cookie);
+  const char* value  = soup_cookie_get_value(cookie);
+  const char* domain = soup_cookie_get_domain(cookie);
+  const char* path   = soup_cookie_get_path(cookie);
+  SoupDate* expires  = soup_cookie_get_expires(cookie);
+  gboolean secure    = soup_cookie_get_secure(cookie);
+  gboolean http_only = soup_cookie_get_http_only(cookie);
+
+  if (new_cookie != NULL) {
+    jumanji_db_cookie_add(jumanji->database, name, value, domain, path,
+        soup_date_to_time_t(expires), (secure == TRUE) ? true : false,
+        (http_only == TRUE) ? true : false);
+  } else {
+    jumanji_db_cookie_remove(jumanji->database, domain, name);
+  }
+}
+
+void
+cb_jumanji_soup_session_request_started(SoupSession* soup_session, SoupMessage*
+    message, SoupSocket* socket, jumanji_t* jumanji)
+{
+  if (jumanji == NULL || jumanji->database== NULL) {
+    return;
+  }
+
+  /* read cookies */
+  girara_list_t* cookies = jumanji_db_cookie_list(jumanji->database);
+  if (cookies != NULL) {
+    jumanji_soup_add_cookies(jumanji, cookies);
+    girara_list_free(cookies);
+  }
+
+  SoupCookieJar* cookie_jar = ((jumanji_soup_t*)jumanji->global.soup)->cookie_jar;
+  SoupURI* uri              = soup_message_get_uri(message);
+  gchar* header             = soup_cookie_jar_get_cookies(cookie_jar, uri, TRUE);
+
+  if (header != NULL) {
+    soup_message_headers_replace(message->request_headers, "Cookie", header);
+    g_free(header);
+  } else {
+    soup_message_headers_remove(message->request_headers, "Cookie");
+  }
+}
+
+void
+jumanji_soup_add_cookies(jumanji_t* jumanji, girara_list_t* cookies)
+{
+  if (jumanji == NULL || cookies == NULL || jumanji->global.soup == 0 ||
+      girara_list_size(cookies) == 0) {
+    return;
+  }
+
+  jumanji_soup_t* soup = (jumanji_soup_t*) jumanji->global.soup;
+
+  if (soup->cookie_jar == NULL) {
+    return;
+  }
+
+  girara_list_iterator_t* iter = girara_list_iterator(cookies);
+  do {
+    SoupCookie* cookie = girara_list_iterator_data(iter);
+    soup_cookie_jar_add_cookie(soup->cookie_jar, cookie);
+  } while (girara_list_iterator_next(iter));
+  girara_list_iterator_free(iter);
 }
 
 void
