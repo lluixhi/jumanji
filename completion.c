c8b388bd5cf814e0cc8e48c756c43988fa24507b
Moritz Lipp <mlq@pwmt.org>
2011-10-31 11:32:39 +0100

    Show search keywords in (tab)open completion

--- completion.c
+++ completion.c
@@ -26,6 +26,38 @@ cc_open(girara_session_t* session, const char* input)
     goto error_free;
   }
 
+  /* search keywords */
+  if (jumanji->global.search_engines != NULL && girara_list_size(jumanji->global.search_engines) > 0) {
+    group = girara_completion_group_create(session, "Search keywords");
+    if (group == NULL) {
+      goto error_free;
+    }
+
+    int number_of_matching = 0;
+    girara_list_iterator_t* iter = girara_list_iterator(jumanji->global.search_engines);
+    do {
+      jumanji_search_engine_t* search_engine = (jumanji_search_engine_t*) girara_list_iterator_data(iter);
+      if (search_engine == NULL) {
+        continue;
+      }
+
+      if (strncmp(input, search_engine->identifier, strlen(input)) == 0) {
+        number_of_matching++;
+        girara_completion_group_add_element(group, search_engine->identifier, search_engine->url);
+        break;
+      }
+    } while (girara_list_iterator_next(iter));
+    girara_list_iterator_free(iter);
+
+    if (number_of_matching != 0) {
+      girara_completion_add_group(completion, group);
+    } else {
+      girara_completion_group_free(group);
+    }
+  }
+
+  group = NULL;
+
   /* search history */
   girara_list_t* bookmark_list = jumanji_db_bookmark_find(jumanji->database, input);
 
